218d31a6155ee6db4f5ae72ad5d5e96d6e032719
Rene Milk <rene.milk@wwu.de>
2016-04-18 12:12:14 +0200

    [travis] add doc deploy infra

---
+++ .travis/deploy_docs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASEDIR=${TRAVIS_BUILD_DIR:-${THISDIR}/..}
+ID=${TRAVIS_TAG:-${TRAVIS_BRANCH}}
+MODULE=${1}
+BUILDDIR=${2}
+
+set -u 
+pushd ${BASEDIR}
+
+git clone git@github.com:dune-community/dune-community.github.io.git site
+
+cd site
+git config user.name "DUNE Community Bot"
+git config user.email "dune-community.bot@wwu.de"
+
+TARGET=docs/${MODULE}/${ID}/
+mkdir -p ${TARGET}
+rsync -a --delete  ${BUILDDIR}/${MODULE}/doc/doxygen/html/ ${TARGET}/
+git add ${TARGET}
+
+git commit -m "Updated documentation for ${MODULE} ${ID}"
+git push
+
+popd
---
+++ .travis/init_sshkey.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+KEY=${1}
+IV=${2}
+RSAKEY=${3}.rsa.enc
+pushd ${DIR}
+openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+popd
