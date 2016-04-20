14005a69ee995c9b8ffb5a12c35aa2f0559a6bdb
Rene Milk <rene.milk@wwu.de>
2016-04-20 09:49:24 +0200

    [travis] increase default swap to 4GB

--- .travis/add_swap.sh
+++ .travis/add_swap.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 set -e
 SWAP=$HOME/swap.img
+MB_SIZE=${1:-2000}
 free
-dd if=/dev/zero of=${SWAP} bs=1024k count=1000
+sudo -E dd if=/dev/zero of=${SWAP} bs=1M count=${MB_SIZE}
 sudo -E chown root:root ${SWAP}
 sudo -E chmod 0600 ${SWAP}
 sudo -E mkswap ${SWAP}
