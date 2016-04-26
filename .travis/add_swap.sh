f8eb71763da65fe95d603ce59380d90ee2a37598
Rene Milk <rene.milk@wwu.de>
2016-04-26 09:36:41 +0200

    [travis] bump defult swap to 4GB

--- .travis/add_swap.sh
+++ .travis/add_swap.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 set -e
 SWAP=$HOME/swap.img
-MB_SIZE=${1:-2000}
-free
+MB_SIZE=${1:-4000}
+free -h
 sudo -E dd if=/dev/zero of=${SWAP} bs=1M count=${MB_SIZE}
 sudo -E chown root:root ${SWAP}
 sudo -E chmod 0600 ${SWAP}
 sudo -E mkswap ${SWAP}
 sudo -E swapon ${SWAP} 
-free
+free -h
