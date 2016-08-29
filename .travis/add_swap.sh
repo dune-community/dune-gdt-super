1dfe310b9a8b2596f1ea927ddbf9675982e8b3da
Rene Milk <rene.milk@wwu.de>
2016-08-29 10:38:34 +0200

    [travis] adds swap and package setup scripts

--- .travis/add_swap.sh
+++ .travis/add_swap.sh
@@ -2,10 +2,10 @@
 set -e
 SWAP=$HOME/swap.img
 MB_SIZE=${1:-4000}
-free -h
 sudo -E dd if=/dev/zero of=${SWAP} bs=1M count=${MB_SIZE}
 sudo -E chown root:root ${SWAP}
 sudo -E chmod 0600 ${SWAP}
 sudo -E mkswap ${SWAP}
 sudo -E swapon ${SWAP} 
-free -h
+echo "echo 1 > /proc/sys/vm/drop_caches" | sudo -E sh
+
