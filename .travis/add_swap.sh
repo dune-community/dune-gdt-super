680b29359bc2879e6a0e257587004cf01b9f39fb
Rene Milk <rene.milk@wwu.de>
2016-08-30 12:10:29 +0200

    [travis] make add_swap drop _all_ caches at the end

--- .travis/add_swap.sh
+++ .travis/add_swap.sh
@@ -6,6 +6,6 @@ sudo -E dd if=/dev/zero of=${SWAP} bs=1M count=${MB_SIZE}
 sudo -E chown root:root ${SWAP}
 sudo -E chmod 0600 ${SWAP}
 sudo -E mkswap ${SWAP}
-sudo -E swapon ${SWAP} 
-echo "echo 1 > /proc/sys/vm/drop_caches" | sudo -E sh
+sudo -E swapon ${SWAP}
+echo "echo 3 > /proc/sys/vm/drop_caches" | sudo -E sh
 
