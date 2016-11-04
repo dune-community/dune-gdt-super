2c9928f7906618fa65401282e0b9daf16ff05dd9
Rene Milk <rene.milk@wwu.de>
2016-11-04 09:51:38 +0100

    [travis] make docker image building variable towards compiler

--- .travis/docker/update_image.py
+++ .travis/docker/update_image.py
@@ -2,11 +2,18 @@
 
 import os
 import subprocess
+import sys
 
+
+branch = sys.argv[2]
+cc = sys.argv[1]
 
 gdt_super_dir = os.path.join(os.path.dirname(__file__), '..', ,'..',)
 dockerfile = os.path.join(os.path.dirname(__file__), 'dune-gdt.docker')
 
 os.chdir(gdt_super_dir)
+cc_mapping = {'gcc-5': 'g++-5', 'gcc-6': 'g++-6', 'clang-3.8': 'clang++-3.8', 'clang-3.9': 'clang++-3.9'])
+
+cxx = cc_mapping[cx]
 subprocess.check_call(['docker', 'build', '-f', '.travis/docker/dune-gdt.docker',
-                       '-t', 'renemilk/dune-testing:gcc5', '.'])
+                       '-t', 'renemilk/dune-testing:{}_{}'.format(cc), '--build-arg cc={}'.format(cc), '--build-arg cxx={}'.format(cxx), '.'])
