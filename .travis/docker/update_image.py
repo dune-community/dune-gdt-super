def70eb30047f901086b587691ed288446e32ff7
Rene Milk <rene.milk@wwu.de>
2016-11-04 10:37:41 +0100

    [docker] fixup update script

--- .travis/docker/update_image.py
+++ .travis/docker/update_image.py
@@ -8,12 +8,13 @@ import sys
 branch = sys.argv[2]
 cc = sys.argv[1]
 
-gdt_super_dir = os.path.join(os.path.dirname(__file__), '..', ,'..',)
+gdt_super_dir = os.path.join(os.path.dirname(__file__), '..', '..',)
 dockerfile = os.path.join(os.path.dirname(__file__), 'dune-gdt.docker')
 
 os.chdir(gdt_super_dir)
-cc_mapping = {'gcc-5': 'g++-5', 'gcc-6': 'g++-6', 'clang-3.8': 'clang++-3.8', 'clang-3.9': 'clang++-3.9'])
+cc_mapping = {'gcc-5': 'g++-5', 'gcc-6': 'g++-6', 'clang-3.8': 'clang++-3.8', 'clang-3.9': 'clang++-3.9'}
 
-cxx = cc_mapping[cx]
+cxx = cc_mapping[cc]
 subprocess.check_call(['docker', 'build', '-f', '.travis/docker/dune-gdt.docker',
-                       '-t', 'renemilk/dune-testing:{}_{}'.format(cc), '--build-arg cc={}'.format(cc), '--build-arg cxx={}'.format(cxx), '.'])
+                       '-t', 'renemilk/dune-testing:{}_{}'.format(cc, branch), '--build-arg', 'cc={}'.format(cc),
+                       '--build-arg', 'cxx={}'.format(cxx), '.'])
