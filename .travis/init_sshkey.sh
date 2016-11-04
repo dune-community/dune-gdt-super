724602a0e928c352776611f534d7659ce6f2bc89
Rene Milk <rene.milk@wwu.de>
2016-11-04 16:05:05 +0100

    [travis] fixup ssh key init in docker container

--- .travis/init_sshkey.sh
+++ .travis/init_sshkey.sh
@@ -7,6 +7,7 @@ KEY=${1}
 IV=${2}
 RSAKEY=${3}.rsa.enc
 pushd ${DIR}
+[ -d ~/.ssh ] || mkdir ~/.ssh
 openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 popd
