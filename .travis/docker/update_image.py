e519d5c9319be92d7a975c9581f9755f31944897
Rene Milk <rene.milk@wwu.de>
2017-07-27 17:07:16 +0200

    [ci] testing images are now commit based
    not branch based

--- .travis/docker/update_image.py
+++ .travis/docker/update_image.py
@@ -8,31 +8,31 @@ import sys
 cc_mapping = {'gcc': 'g++', 'clang': 'clang++'}
 thisdir = os.path.dirname(os.path.abspath(__file__))
 
-def update(branch, cc):
+def update(commit, cc):
     gdt_super_dir = os.path.join(thisdir, '..', '..',)
     dockerfile = os.path.join(thisdir, 'dune-gdt-testing', 'Dockerfile')
 
     os.chdir(gdt_super_dir)
 
     cxx = cc_mapping[cc]
-    branch = branch.replace('/', '_')
+    commit = commit.replace('/', '_')
     repo = 'dunecommunity/dune-gdt-testing_{}'.format(cc)
     subprocess.check_call(['docker', 'build', '--no-cache=true', '-f', dockerfile,
-                        '-t', '{}:{}'.format(repo, branch), '--build-arg', 'cc={}'.format(cc),
-                        '--build-arg', 'cxx={}'.format(cxx), '--build-arg', 'branch={}'.format(branch),
+                        '-t', '{}:{}'.format(repo, commit), '--build-arg', 'cc={}'.format(cc),
+                        '--build-arg', 'cxx={}'.format(cxx), '--build-arg', 'commit={}'.format(commit),
                         '.'])
     subprocess.check_call(['docker', '--log-level="debug"', 'push', repo])
 
 if __name__ == '__main__':
     if len(sys.argv) > 2:
         ccs = [sys.argv[1]]
-        branches = [sys.argv[2]]
+        commmits = [sys.argv[2]]
     else:
         ccs = list(cc_mapping.keys())
-        branches = ['master']
+        commits = ['master']
 
     subprocess.check_call(['docker', 'pull', 'dunecommunity/testing-base:latest'])
-    for b in branches:
+    for b in commits:
         for c in ccs:
             update(b, c)
     subprocess.check_call(['docker', '--log-level="debug"', 'images'])
