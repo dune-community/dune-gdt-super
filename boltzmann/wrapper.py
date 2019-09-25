Fix error calculation in cellmodel POD, update pymor

boltzmann/wrapper.py
@@ -602,26 +602,12 @@ def calculate_cellmodel_trajectory_error(final_modes_pfield, final_modes_ofield,
     while not solver.finished():
         print("timestep: ", n)
         next_vectors_pfield, next_vectors_ofield, next_vectors_stokes = solver.next_n_timesteps(1, dt)
-        err_pfield += np.sum(
-            np.sqrt(
-                next_vectors_pfield.dot(
-                    solver.apply_pfield_product_operator(
-                        next_vectors_pfield -
-                        final_modes_pfield.lincomb(next_vectors_pfield.dot(final_modes_pfield))))))
-        print("error_pfield_done: ", n)
-        err_ofield += np.sum(
-            np.sqrt(
-                next_vectors_ofield.dot(
-                    solver.apply_ofield_product_operator(
-                        next_vectors_ofield -
-                        final_modes_ofield.lincomb(next_vectors_ofield.dot(final_modes_ofield))))))
-        print("error_ofield_done: ", n)
-        err_stokes += np.sum(
-            np.sqrt(
-                next_vectors_stokes.dot(
-                    solver.apply_stokes_product_operator(
-                        next_vectors_stokes -
-                        final_modes_stokes.lincomb(next_vectors_stokes.dot(final_modes_stokes))))))
+        pfield_residual = next_vectors_pfield - final_modes_pfield.lincomb(next_vectors_pfield.dot(solver.apply_pfield_product_operator(final_modes_pfield)))
+        err_pfield += np.sum(pfield_residual.dot(solver.apply_pfield_product_operator(pfield_residual)))
+        ofield_residual = next_vectors_ofield - final_modes_ofield.lincomb(next_vectors_ofield.dot(solver.apply_ofield_product_operator(final_modes_ofield)))
+        err_ofield += np.sum(ofield_residual.dot(solver.apply_ofield_product_operator(ofield_residual)))
+        stokes_residual = next_vectors_stokes - final_modes_stokes.lincomb(next_vectors_stokes.dot(solver.apply_stokes_product_operator(final_modes_stokes)))
+        err_stokes += np.sum(stokes_residual.dot(solver.apply_stokes_product_operator(stokes_residual)))
         n += 1;
     return err_pfield, err_ofield, err_stokes
 
@@ -642,15 +628,14 @@ def calculate_mean_cellmodel_projection_error(final_modes_pfield,
                                               with_half_steps=True):
     trajectory_error_pfield, trajectory_error_ofield, trajectory_error_stokes = calculate_cellmodel_trajectory_error(
         final_modes_pfield, final_modes_ofield, final_modes_stokes, testcase, t_end, dt, grid_size_x, grid_size_y, mu)
-    print("trajcetory_error done")
     trajectory_errors_pfield = mpi_wrapper.comm_world.gather(trajectory_error_pfield, root=0)
     trajectory_errors_ofield = mpi_wrapper.comm_world.gather(trajectory_error_ofield, root=0)
     trajectory_errors_stokes = mpi_wrapper.comm_world.gather(trajectory_error_stokes, root=0)
     err_pfield = err_ofield = err_stokes = 0
     if mpi_wrapper.rank_world == 0:
-        err_pfield = np.sum(trajectory_errors_pfield) / total_num_snapshots_pfield
-        err_ofield = np.sum(trajectory_errors_ofield) / total_num_snapshots_ofield
-        err_stokes = np.sum(trajectory_errors_stokes) / total_num_snapshots_stokes
+        err_pfield = np.sqrt(np.sum(trajectory_errors_pfield))
+        err_ofield = np.sqrt(np.sum(trajectory_errors_ofield))
+        err_stokes = np.sqrt(np.sum(trajectory_errors_stokes))
     return err_pfield, err_ofield, err_stokes
 
 

cellmodel_pod.py
@@ -20,7 +20,7 @@ def calculate_pod(result, product, mpi):
     elapsed_pod = 0
     start = timer()
     if mpi.rank_world == 0:
-        result, svals = pod(result, product=product, atol=0., rtol=0., l2_err=tol * np.sqrt(total_num_snapshots))
+        result, svals = pod(result, product=product, atol=0., rtol=0., l2_err=tol)
         elapsed_pod = timer() - start
     return result, svals, elapsed_pod, total_num_snapshots
 
@@ -35,7 +35,7 @@ def cellmodel_pod(testcase, t_end, dt, grid_size_x, grid_size_y, tol, logfile=No
 
     # calculate Boltzmann problem trajectory
     start = timer()
-    snapshots_pfield, snapshots_ofield, snapshots_stokes = solver.solve(dt)
+    snapshots_pfield, snapshots_ofield, snapshots_stokes = solver.solve(dt, True, dt, "result_" + str(mu))
     mpi.comm_world.Barrier()
     elapsed_data_gen = timer() - start
 
@@ -46,13 +46,10 @@ def cellmodel_pod(testcase, t_end, dt, grid_size_x, grid_size_y, tol, logfile=No
 
     [result_pfield, svals_pfield, elapsed_pod_pfield,
      total_num_snapshots_pfield] = calculate_pod(snapshots_pfield, pfield_product, mpi)
-    print("pfield pod done")
     [result_ofield, svals_ofield, elapsed_pod_ofield,
      total_num_snapshots_ofield] = calculate_pod(snapshots_ofield, ofield_product, mpi)
-    print("pfield pod done")
     [result_stokes, svals_stokes, elapsed_pod_stokes,
      total_num_snapshots_stokes] = calculate_pod(snapshots_stokes, stokes_product, mpi)
-    print("pfield pod done")
 
     # write statistics to file
     if logfile is not None and mpi.rank_world == 0:
@@ -80,7 +77,7 @@ if __name__ == "__main__":
     grid_size_x = int(sys.argv[4])
     grid_size_y = int(sys.argv[5])
     tol = float(sys.argv[6])
-    filename = "cellmodel_POD_gridsize_%d_tol_%f" % (grid_size_y, tol)
+    filename = "cellmodel_POD_grid_%dx%d_tol_%f" % (grid_size_x, grid_size_y, tol)
     logfile = open(filename, "a")
     [
         final_modes_pfield, _, total_num_snapshots_pfield, final_modes_ofield, _, total_num_snapshots_ofield,