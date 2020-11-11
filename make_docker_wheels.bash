b78d849f86a7c3fffc1c7204f4de287b262fc078
René Fritze <rene.fritze@wwu.de>
2020-11-11 15:38:46 +0100

    bump xt+gdt

---
+++ make_docker_wheels.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+THISDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd -P )"
+
+set -e
+# default command is "build-wheels.sh"
+# this deletes testtols and uggrid source dirs
+docker run -e DUNE_SRC_DIR=/home/dxt/src -v ${THISDIR}:/home/dxt/src \
+  -it dunecommunity/manylinux-2014:ddb4b80
+
+docker run -v ${THISDIR}/docker/wheelhouse/final:/wheelhouse:ro -it pymor/testing_py3.8:latest \
+  bash -c "pip install /wheelhouse/dune* && python -c 'from dune.xt import *; from dune.gdt import *'"
+
+echo '************************************'
+echo Wheels are in ${THISDIR}/docker/wheelhouse/final
