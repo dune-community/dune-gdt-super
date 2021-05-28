165ade1736c78043897d1c2630075e03a33d177c
René Fritze <rene.fritze@wwu.de>
2021-05-28 11:52:06 +0200

    tweak wheel script to allow setting {u,g}id
    to make rootless + non-1000 user run easier

--- make_docker_wheels.bash
+++ make_docker_wheels.bash
@@ -4,12 +4,17 @@ ML_TAG=6055ab2
 
 THISDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd -P )"
 
-set -e
+# for rootless/podman execution set both to 0
+LOCAL_UID=${LOCAL_UID:-$(id -u)}
+LOCAL_GID=${LOCAL_GID:-$(id -g)}
+
+set -eu
 # default command is "build-wheels.sh"
 # this deletes testtols and uggrid source dirs
 docker run -e DUNE_SRC_DIR=/home/dxt/src -v ${THISDIR}:/home/dxt/src \
-  -it dunecommunity/manylinux-2014:${ML_TAG}
+  -e LOCAL_GID=${LOCAL_GID} -e LOCAL_UID=${LOCAL_UID} -it dunecommunity/manylinux-2014:${ML_TAG}
 
+# makes sure wheels are importable
 docker run -v ${THISDIR}/docker/wheelhouse/final:/wheelhouse:ro -it pymor/testing_py3.8:latest \
   bash -c "pip install /wheelhouse/dune* && python -c 'from dune.xt import *; from dune.gdt import *'"
 
