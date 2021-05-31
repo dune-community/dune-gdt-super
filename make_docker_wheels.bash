76eeaaa0c3fab4cacda2c489b7761cef8a89b534
René Fritze <rene.fritze@wwu.de>
2021-05-31 14:32:04 +0200

    [ci] switch to per-python wheelbuilder images

--- make_docker_wheels.bash
+++ make_docker_wheels.bash
@@ -7,15 +7,20 @@ THISDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd -P )"
 # for rootless/podman execution set both to 0
 LOCAL_UID=${LOCAL_UID:-$(id -u)}
 LOCAL_GID=${LOCAL_GID:-$(id -g)}
+PYTHON_VERSION=${GDT_PYTHON_VERSION:-3.8}
 
 set -eu
+
+IMAGE=dunecommunity/manylinux-2014_py${PYTHON_VERSION}:${ML_TAG}
+TEST_IMAGE=pymor/testing_py${PYTHON_VERSION}:latest
+
 # default command is "build-wheels.sh"
 # this deletes testtols and uggrid source dirs
 docker run -e DUNE_SRC_DIR=/home/dxt/src -v ${THISDIR}:/home/dxt/src \
-  -e LOCAL_GID=${LOCAL_GID} -e LOCAL_UID=${LOCAL_UID} -i dunecommunity/manylinux-2014:${ML_TAG}
+  -e LOCAL_GID=${LOCAL_GID} -e LOCAL_UID=${LOCAL_UID} -i ${IMAGE}
 
 # makes sure wheels are importable
-docker run -v ${THISDIR}/docker/wheelhouse/final:/wheelhouse:ro -i pymor/testing_py3.8:latest \
+docker run -v ${THISDIR}/docker/wheelhouse/final:/wheelhouse:ro -i ${TEST_IMAGE} \
   bash -c "pip install /wheelhouse/dune* && python -c 'from dune.xt import *; from dune.gdt import *'"
 
 echo '************************************'
