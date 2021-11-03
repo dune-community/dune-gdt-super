4627bd4f6515ffce6d911bf5897721d8281ff0c1
René Fritze <rene.fritze@wwu.de>
2021-11-03 10:28:34 +0100

    [wheelbuilder] conditionally add a terminal to docker run
    If one executes the script locally one should be able to abort it now
    with ctrl+c

--- make_docker_wheels.bash
+++ make_docker_wheels.bash
@@ -13,17 +13,23 @@ set -eu
 
 IMAGE=zivgitlab.wwu.io/ag-ohlberger/dune-community/docker/manylinux-2014_py${PYTHON_VERSION}:${ML_TAG}
 TEST_IMAGE=pymor/testing_py${PYTHON_VERSION}:latest
+# check if we a have TTY first, else docker run would throw an error
+if [ -t 1 ] ; then 
+  DT="-t"
+else 
+  DT=""
+fi
 
 [[ -e ${THISDIR}/docker ]] || mkdir -p ${THISDIR}/docker
 export DOCKER_ENVFILE=${THISDIR}/docker/env
 python3 ./.ci/shared/scripts/make_env_file.py
 # default command is "build-wheels.sh"
 # this deletes testtols and uggrid source dirs
-docker run --env-file=${DOCKER_ENVFILE} -e DUNE_SRC_DIR=/home/dxt/src -v ${THISDIR}:/home/dxt/src \
+docker run ${DT} --env-file=${DOCKER_ENVFILE} -e DUNE_SRC_DIR=/home/dxt/src -v ${THISDIR}:/home/dxt/src \
   -e LOCAL_GID=${LOCAL_GID} -e LOCAL_UID=${LOCAL_UID} -i ${IMAGE}
 
 # makes sure wheels are importable
-docker run -v ${THISDIR}/docker/wheelhouse/final:/wheelhouse:ro -i ${TEST_IMAGE} \
+docker run ${DT} -v ${THISDIR}/docker/wheelhouse/final:/wheelhouse:ro -i ${TEST_IMAGE} \
   bash -c "pip install /wheelhouse/dune* && python -c 'from dune.xt import *; from dune.gdt import *'"
 
 echo '************************************'
