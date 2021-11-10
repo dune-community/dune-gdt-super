701c86bf059b125366f2ed0911245e6806b1b9c7
Felix Schindler <felix.schindler@wwu.de>
2021-11-10 18:44:12 +0100

    [wheels] extend make script capabilities

--- make_docker_wheels.bash
+++ make_docker_wheels.bash
@@ -5,8 +5,10 @@ ML_TAG=e6c39beb1ef4f1c98513e80c683c79815f5d38d0
 THISDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd -P )"
 
 # for rootless/podman execution set both to 0
+LOCAL_USER=${LOCAL_USER:$USER}
 LOCAL_UID=${LOCAL_UID:-$(id -u)}
 LOCAL_GID=${LOCAL_GID:-$(id -g)}
+EXEC=${EXEC}
 PYTHON_VERSION=${GDT_PYTHON_VERSION:-3.8}
 
 set -eu
@@ -26,7 +28,7 @@ python3 ./.ci/shared/scripts/make_env_file.py
 # default command is "build-wheels.sh"
 # this deletes testtols and uggrid source dirs
 docker run ${DT} --env-file=${DOCKER_ENVFILE} -e DUNE_SRC_DIR=/home/dxt/src -v ${THISDIR}:/home/dxt/src \
-  -e LOCAL_GID=${LOCAL_GID} -e LOCAL_UID=${LOCAL_UID} -i ${IMAGE}
+  -e LOCAL_USER=${LOCAL_USER} -e LOCAL_GID=${LOCAL_GID} -e LOCAL_UID=${LOCAL_UID} -i ${IMAGE} ${EXEC}
 
 # makes sure wheels are importable
 docker run ${DT} -v ${THISDIR}/docker/wheelhouse/final:/wheelhouse:ro -i ${TEST_IMAGE} \
