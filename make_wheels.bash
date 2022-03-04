9153744b6548011b5f5327e635915e567c4ff981
René Fritze <rene.fritze@wwu.de>
2022-03-04 09:30:00 +0100

    [ci] drop the dind setup for wheels

---
+++ make_wheels.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+
+THISDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd -P )"
+md=${1}
+shift
+
+PIP_CONFIG_FILE=${PIP_CONFIG_FILE:-${HOME}/.config/pip/pip.conf}
+[[ -d $(dirname ${PIP_CONFIG_FILE})  ]]  || mkdir -p $(dirname ${PIP_CONFIG_FILE})
+sed "s;PYPI_INDEX_URL;${GITLAB_PYPI}/simple;g" ${THISDIR}/.ci/pip.conf > ${PIP_CONFIG_FILE}
+python3 -m pip install twine
+
+set -eu
+  
+build-wheels.sh ${md}
+
+echo '************************************'
+echo Wheels are in ${WHEEL_DIR}/final
