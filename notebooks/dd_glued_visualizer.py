f1e2499e8b67a54039eb5240983305f38ef55ea8
Tim Keil <tim.keil@wwu.de>
2022-08-05 17:02:44 +0200

    small update to visualization

--- notebooks/dd_glued_visualizer.py
+++ notebooks/dd_glued_visualizer.py
@@ -3,10 +3,10 @@ from dune.gdt import visualize_discrete_functions_on_dd_grid, DiscreteFunction
 def visualize_dd_functions(dd_grid, local_spaces, u, subdomains=None, name='u_ipdg'):
     discrete_functions = []
     subdomains = subdomains or list(range(dd_grid.num_subdomains))
-    for ss in subdomains:
-        u_list_vector_array = u.block(ss)
-        u_ss_istl = u_list_vector_array._list[0].real_part.impl
-        u_ss = DiscreteFunction(local_spaces[ss], u_ss_istl, name=name)
-        discrete_functions.append(u_ss)
+    for I in subdomains:
+        u_list_vector_array = u.block(I)
+        u_I_istl = u_list_vector_array._list[0].real_part.impl
+        u_I = DiscreteFunction(local_spaces[I], u_I_istl, name=name)
+        discrete_functions.append(u_I)
 
     _ = visualize_discrete_functions_on_dd_grid(discrete_functions, dd_grid, subdomains)
